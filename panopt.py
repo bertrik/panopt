9982c5c81e1643b70f0f8f279145d719c7c4f93c
Bertrik Sikken <bertrik@sikken.nl>
2017-06-02 00:55:40 +0200

    Refactor things a bit: * create a class for each camera * pre-render camera labels * update camera mapping (cam3 added)

--- panopt.py
+++ panopt.py
@@ -14,14 +14,23 @@ import time
 import sys
 import ctypes
 import urllib2
+import json
 from sdl2 import *
 from sdl2.sdlimage import *
+from sdl2.sdlttf import *
 
 # screen and tile dimensions
 SCREEN_W = 1280
 SCREEN_H = 960
 TILE_W = SCREEN_W / 3
 TILE_H = SCREEN_H / 3
+
+class Camera(object):
+    def __init__(self, x, y, label, url):
+        self.x = x
+        self.y = y
+        self.label = label
+        self.url = url
 
 # reads one image from the source
 def readframe(url):
@@ -31,20 +40,36 @@ def readframe(url):
     except Exception:
         return None
 
-# updates the main screen with one image
-def update(window, x, y, url):
-    image = readframe(url)
-    if image is None:
+def renderText(text, size):
+    font = TTF_OpenFont("VCR_OSD_MONO_1.001.ttf", size)
+    surface = TTF_RenderText_Shaded(font, text, SDL_Color(255,255,255), SDL_Color(0,0,0))
+    TTF_CloseFont(font);
+    
+    return surface
+
+# renders one camera onto the window
+def renderCamera(window, camera):
+    # get JPG
+    jpeg = readframe(camera.url)
+    if jpeg is None:
         return
-    rwops = SDL_RWFromMem(image, sys.getsizeof(image))
+    rwops = SDL_RWFromMem(jpeg, sys.getsizeof(jpeg))
     image = IMG_LoadTyped_RW(rwops, True, "JPG")
-    rect = SDL_Rect(TILE_W * x, TILE_H * y, TILE_W, TILE_H)
+
+    # blit scaled JPG
+    rect = SDL_Rect(TILE_W * camera.x, TILE_H * camera.y, TILE_W, TILE_H)
     SDL_BlitScaled(image, None, SDL_GetWindowSurface(window), rect)
     SDL_FreeSurface(image)
+    
+    # draw text over it
+    SDL_BlitSurface(camera.osd, None, SDL_GetWindowSurface(window), rect)
+
     SDL_UpdateWindowSurface(window)
 
 def main():
     SDL_Init(SDL_INIT_VIDEO)
+    TTF_Init()
+
     window = SDL_CreateWindow(b"Panopticon",
                               SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                               SCREEN_W, SCREEN_H, SDL_WINDOW_SHOWN)
@@ -55,19 +80,36 @@ def main():
     lasttime = starttime
     file = 0
 
-    while True:
-        # draw all rectangles into buffer
-        update(window, 0, 0, "http://localhost:8000/cgi-bin/nph-mjgrab?2")
-        update(window, 1, 0, "http://localhost:8000/cgi-bin/nph-mjgrab?6")
-        update(window, 2, 0, "http://localhost:8000/cgi-bin/nph-mjgrab?7")
+    cameras = list()
+    cameras.append(Camera(0, 0, "KEUKEN", "http://localhost:8000/cgi-bin/nph-mjgrab?2"))
+    cameras.append(Camera(1, 0, "WERKPLAATS", "http://localhost:8000/cgi-bin/nph-mjgrab?6"))
+    cameras.append(Camera(2, 0, "ACHTERDEUR", "http://localhost:8000/cgi-bin/nph-mjgrab?7"))
 
-        update(window, 0, 1, "http://localhost:8000/cgi-bin/nph-mjgrab?5")
-        update(window, 1, 1, "http://localhost:8000/cgi-bin/nph-mjgrab?4")
-        update(window, 2, 1, "http://localhost:8000/cgi-bin/nph-mjgrab?1")
+    cameras.append(Camera(0, 1, "SPACE", "http://localhost:8000/cgi-bin/nph-mjgrab?3"))
+    cameras.append(Camera(1, 1, "SPACE", "http://localhost:8000/cgi-bin/nph-mjgrab?4"))
+    cameras.append(Camera(2, 1, "3D-PRINTER", "http://localhost:8000/cgi-bin/nph-mjgrab?1"))
 
-        update(window, 0, 2, "http://localhost:8000/cgi-bin/nph-mjgrab?10")
-        update(window, 1, 2, "http://localhost:8000/cgi-bin/nph-mjgrab?9")
-        update(window, 2, 2, "http://localhost:8000/cgi-bin/nph-mjgrab?8")
+    cameras.append(Camera(0, 2, "INRIT", "http://localhost:8000/cgi-bin/nph-mjgrab?10"))
+    cameras.append(Camera(1, 2, "PARKEER", "http://localhost:8000/cgi-bin/nph-mjgrab?9"))
+    cameras.append(Camera(2, 2, "PARKEER", "http://localhost:8000/cgi-bin/nph-mjgrab?8"))
+    
+    # prerender OSD
+    for camera in cameras:
+        camera.osd = renderText(camera.label, 30)
+    
+    running = True
+    while running:
+        for camera in cameras:
+            if not running:
+                break;
+
+            # draw one cam
+            renderCamera(window, camera);
+    
+            # check for quit button
+            while SDL_PollEvent(ctypes.byref(event)):
+                if event.type == SDL_QUIT:
+                    running = False
     
         # calculate fps
         iterations = iterations + 1
@@ -76,11 +118,6 @@ def main():
             fps = iterations / delta_t
             print(fps)
 
-        # check for quit button
-        while SDL_PollEvent(ctypes.byref(event)):
-            if event.type == SDL_QUIT:
-                break
-    
     SDL_DestroyWindow(window)
     SDL_Quit()
     return 0
