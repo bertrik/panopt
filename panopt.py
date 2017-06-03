d8e85b1d5f73ab30c947d5e72f3644683eac168a
Bertrik Sikken <bertrik@sikken.nl>
2017-06-04 01:03:50 +0200

    Tile cameras as 4x3 with one camera double size into a full screen window.

--- panopt.py
+++ panopt.py
@@ -19,16 +19,11 @@ from sdl2 import *
 from sdl2.sdlimage import *
 from sdl2.sdlttf import *
 
-# screen and tile dimensions
-SCREEN_W = 1280
-SCREEN_H = 960
-TILE_W = SCREEN_W / 3
-TILE_H = SCREEN_H / 3
-
 class Camera(object):
-    def __init__(self, x, y, label, url):
+    def __init__(self, x, y, scale, label, url):
         self.x = x
         self.y = y
+        self.scale = scale
         self.label = label
         self.url = url
 
@@ -55,6 +50,12 @@ def renderText(text, size):
 
 # renders one camera onto the window
 def renderCamera(window, camera):
+    # get window properties
+    cw = ctypes.c_int()
+    ch = ctypes.c_int()
+    SDL_GetWindowSize(window, ctypes.byref(cw), ctypes.byref(ch))
+    w,h = cw.value,ch.value
+
     # get JPG
     jpeg = readframe(camera.url)
     if jpeg is None:
@@ -63,7 +64,9 @@ def renderCamera(window, camera):
     image = IMG_LoadTyped_RW(rwops, True, "JPG")
 
     # blit scaled JPG
-    rect = SDL_Rect(TILE_W * camera.x, TILE_H * camera.y, TILE_W, TILE_H)
+    x = w * camera.x / 4
+    y = h * camera.y / 3
+    rect = SDL_Rect(x, y, w * camera.scale / 4, h * camera.scale / 3)
     SDL_BlitScaled(image, None, SDL_GetWindowSurface(window), rect)
     SDL_FreeSurface(image)
     
@@ -77,8 +80,12 @@ def main():
     TTF_Init()
 
     window = SDL_CreateWindow(b"Panopticon",
-                              SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                              SCREEN_W, SCREEN_H, SDL_WINDOW_SHOWN)
+                              SDL_WINDOWPOS_CENTERED, 
+                              SDL_WINDOWPOS_CENTERED,
+                              1024, 
+                              768, 
+                              SDL_WINDOW_SHOWN)
+    SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN_DESKTOP)
     
     event = SDL_Event()
     iterations = 0
@@ -87,17 +94,18 @@ def main():
     file = 0
 
     cameras = list()
-    cameras.append(Camera(0, 0, "KEUKEN", "http://localhost:8000/cgi-bin/nph-mjgrab?2"))
-    cameras.append(Camera(1, 0, "WERKPLAATS", "http://localhost:8000/cgi-bin/nph-mjgrab?6"))
-    cameras.append(Camera(2, 0, "ACHTERDEUR", "http://localhost:8000/cgi-bin/nph-mjgrab?7"))
+    cameras.append(Camera(0, 0, 1, "ACHTERDEUR", "http://localhost:8000/cgi-bin/nph-mjgrab?7"))
+    cameras.append(Camera(0, 1, 1, "WERKPLAATS", "http://localhost:8000/cgi-bin/nph-mjgrab?6"))
+    cameras.append(Camera(0, 2, 1, "KEUKEN", "http://localhost:8000/cgi-bin/nph-mjgrab?2"))
 
-    cameras.append(Camera(0, 1, "SPACE", "http://localhost:8000/cgi-bin/nph-mjgrab?3"))
-    cameras.append(Camera(1, 1, "SPACE", "http://localhost:8000/cgi-bin/nph-mjgrab?4"))
-    cameras.append(Camera(2, 1, "3D-PRINTER", "http://localhost:8000/cgi-bin/nph-mjgrab?1"))
+    cameras.append(Camera(1, 0, 2, "SPACE", "http://localhost:8000/cgi-bin/nph-mjgrab?4"))
+    cameras.append(Camera(1, 2, 1, "SPACE", "http://localhost:8000/cgi-bin/nph-mjgrab?3"))
 
-    cameras.append(Camera(0, 2, "INRIT", "http://localhost:8000/cgi-bin/nph-mjgrab?10"))
-    cameras.append(Camera(1, 2, "PARKEER", "http://localhost:8000/cgi-bin/nph-mjgrab?9"))
-    cameras.append(Camera(2, 2, "PARKEER", "http://localhost:8000/cgi-bin/nph-mjgrab?8"))
+    cameras.append(Camera(2, 2, 1, "3D-PRINTER", "http://localhost:8000/cgi-bin/nph-mjgrab?1"))
+
+    cameras.append(Camera(3, 0, 1, "PARKEER", "http://localhost:8000/cgi-bin/nph-mjgrab?9"))
+    cameras.append(Camera(3, 1, 1, "PARKEER", "http://localhost:8000/cgi-bin/nph-mjgrab?8"))
+    cameras.append(Camera(3, 2, 1, "INRIT", "http://localhost:8000/cgi-bin/nph-mjgrab?10"))
     
     # prerender OSD
     for camera in cameras:
